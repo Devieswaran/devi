e865c34ab7664f8a772e89cac11de10dc3bde7b5
Devieswaran <devi.eswaran98@gmail.com>
2017-06-29 16:15:18 +0530

    Create progr.c

---
+++ progr.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("\n enter the number");
+scanf("%d",&a);
+if(a>0)
+{
+printf("\n positive");
+}
+if else(a<0)
+{
+printf("\n negative");
+}
+else
+{
+printf("zero");
+}
+return 0;
+}
